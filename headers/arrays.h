66f0a95f3f672b98b608d9a26d7fd65990db837d
DankoPop <45147673+DankoPop@users.noreply.github.com>
2018-11-18 17:50:59 +0100

    First commit

---
+++ headers/arrays.h
@@ -0,0 +1,8 @@
+#ifndef __arrays_h__
+#define __arrays_h__
+
+// Funkcija za ispis niza
+void readArrayElements(int arr[], int arrLength);
+void printArrayElement(int arr[], int arrLength);	
+
+#endif
